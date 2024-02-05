822c5ad655f5ce6bb767a622616529e746f764ac
valdenio cosme <valdenio.junior@hotmail.com>
2024-02-05 18:17:36 -0300

    Exercicios Basicos de listas usando JavaScript

---
+++ desafioLista.js
@@ -0,0 +1,15 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagemDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagemDeProgramacao.push(['Java', 'Ruby', 'GoLang']);
+
+let listaNomes = ['Valdenio','Mateus','Cleyson'];
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+console.log(listaNomes[0]);
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1]);
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2]);
